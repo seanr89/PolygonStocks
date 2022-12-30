2e78f54207cfc359fb333174da000c7a55392d47
Sean Rafferty <srafferty89@gmail.com>
2022-12-30 16:01:05 +0000

    New console and app work

---
+++ src/PolyConsole/Program.cs
@@ -0,0 +1,11 @@
+﻿using Spectre.Console;
+
+AnsiConsole.Markup("[underline red]Hello[/] World!");
+
+if (!AnsiConsole.Confirm("Run prompt example?"))
+{
+    AnsiConsole.MarkupLine("Ok... :(");
+    return false;
+}
+
+return true;
