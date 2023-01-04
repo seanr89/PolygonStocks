f05144f31baf778fd8c08cd87eda59426bd84db0
Sean Rafferty <srafferty89@gmail.com>
2023-01-04 23:12:14 +0000

    prep work

--- src/PolyConsole/Program.cs
+++ src/PolyConsole/Program.cs
@@ -1,5 +1,11 @@
 ﻿using Spectre.Console;
 
+// Build a config object, using env vars and JSON providers.
+IConfiguration config = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json")
+    .AddEnvironmentVariables()
+    .Build();
+
 AnsiConsole.Markup("[underline red]Hello[/] World!");
 
 if (!AnsiConsole.Confirm("Run prompt example?"))
